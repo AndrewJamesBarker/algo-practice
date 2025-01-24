79befba7f501d5ddb27443b2d3c3dd3b7cce45ca
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2025-01-23 22:20:42 -0500

    restructure dir and add js dir wth a roman num solution

--- 238-Product-of-Array-Except-Self.py
+++ 238-Product-of-Array-Except-Self.py
@@ -1,23 +1,32 @@
+# class Solution:
+#     def productExceptSelf(self, nums: list[int]) -> list[int]:
+    
+#         length = len(nums)
+#         left_mult = 1
+#         right_mult = 1
+#         left_arr = [0] * len(nums)
+#         right_arr = [0] * len(nums)
+#         for i in range(length):
+#             j = -i -1
+#             left_arr[i] = left_mult
+#             left_mult *= nums[i]
+#             right_arr[j] = right_mult
+#             right_mult *= nums[j]
+#         ans = [left_arr[i] * right_arr[i] for i in range(length)]
+#         return ans
+
 class Solution:
     def productExceptSelf(self, nums: list[int]) -> list[int]:
-        # ans = [0] * len(nums)
-        # i = 0
-        # length = len(nums)
-        # while i < length:
-        #     product = 1
-        #     j = 0
-        #     for j in range(length):
-        #         if j != i:
-        #             product *= nums[j]
-        #             ans[i] = product
-        #     i += 1
-        # return ans
-    
         length = len(nums)
-        left_mult = 1
-        right_mult = 1
-        left_arr = [0] * len(nums)
-        right_arr = [0] * len(nums)
-        
+        result = [1] * length
+        prefix = 1
+        for i in range(length):
+            result[i] = prefix
+            prefix *= nums[i]
+        suffix = 1
+        for i in range(length-1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+        return result
 
 print(Solution().productExceptSelf([1,2,3,4])) # [24,12,8,6]
---
+++ js-versions/romanNumeral.js
@@ -0,0 +1,36 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+  const romNums = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+  };
+
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    let current = romNums[s[i]];
+    let next = romNums[s[i + 1]];
+    if (current < next) {
+      result += next - current;
+      i++;
+    } else {
+      result += current;
+    }
+  }    
+  console.log(result); 
+
+  return result; 
+  
+
+
+};
+
+// Invoke the function to see the output
+romanToInt("III"); // Example input
